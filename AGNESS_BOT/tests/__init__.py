2dc76351d7f684134b554a9d82d5addab6fa3f79
Nitin Sharma <ns290670@gmail.com>
2020-11-18 08:55:29 +0530

    test removed

--- AGNESS_BOT/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import re
-import base64
-SEEK_REGEX = r'^[+|-]([1-9]|[1-5][0-9]|60)$'
-
-# print(re.match(SEEK_REGEX, '+7').groups())
-
-print(base64.urlsafe_b64encode(b'www.google.com').decode('utf-8'))
